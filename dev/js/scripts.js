8c320de427ed70023876e7746cc87188aa442205
AJ-22a <anthonycortese22@icloud.com>
2025-06-13 19:13:20 -0400

    Changed prepros config to properly compile java file. Added AOS animations to website, made some small other design changes.

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,3 +1,14 @@
+//AOS Import
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init({
+  duration: 1000,
+  once: false,
+  mirror: true
+});
+
+// Hamburger Menu Code
 document.addEventListener("DOMContentLoaded", function () {
   const burger = document.getElementById("burger");
   const navLinks = document.getElementById("nav-links");
